c7b508fccd5edf09b8ae53c04dc4e40b0ab0f662
[A10] 박수정 <d-aoo@naver.com>
2024-06-13 04:20:56 +0900

    ✨ Feat: 에러 처리 미들웨어

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {swaggerUi, specs} = require('./utils/swagger');
+const { swaggerUi, specs } = require('./utils/swagger');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 
@@ -15,6 +15,23 @@ app.get('/', (req, res, next) => {
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use('/api/dashboard', dashboard);
 app.use('/dashboard-scatter', dashboardScatter);
+
+// 에러 처리
+app.use((error, req, res, next) => {
+    const { name, message, status, data } = error;
+    if (status >= 500) {
+        console.error(name, message);
+        res.status(status).json({
+            error: '서버 내부에서 에러가 발생하였습니다.',
+            data,
+        });
+        return;
+    }
+    res.status(status).json({
+        error: message,
+        data,
+    });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`server listening on port ${process.env.PORT}`);
