7965d00e4a6dd5b451a7dd23051f019528685d00
[A10] 박수정 <d-aoo@naver.com>
2024-06-18 12:17:33 +0900

    ✨ Feat: 공원 1개에 대한 지도 API

--- src/models/map.js
+++ src/models/map.js
@@ -5,19 +5,33 @@ Author : 박수정
 History
 Date        Author   Status     Description
 2024.06.15  박수정   Created
+2024.06.15  박수정   Modified   공원 전체에 대한 지도 API 추가
 2024.06.15  박수정   Modified   지도 API 분리 - routes, service, model
+2024.06.18  박수정   Modified   공원 1개에 대한 지도 API 추가
 */
 
 import db from '../models/psql.js';
 
 class MapModel {
-    static async getMap() {
+    // 공원 전체 조회
+    static async getAllMap() {
         return await db.query(`
-                SELECT p.name, pr.address, pr.latitude, pr.longitude
+                SELECT p.id, p.name, pr.address, pr.latitude, pr.longitude
                 FROM park p
                 JOIN park_region pr
                     ON p.park_region_id = pr.id
             `);
+    }
+
+    // 공원 1개 조회
+    static async getOneMap(id) {
+        return await db.query(`
+            SELECT p.id, p.name, pr.address, pr.latitude, pr.longitude
+            FROM park p
+            JOIN park_region pr
+                ON p.park_region_id = pr.id
+            WHERE p.id = ${id}
+        `);
     }
 }
 
--- src/services/map.js
+++ src/services/map.js
@@ -5,28 +5,52 @@ Author : 박수정
 History
 Date        Author   Status     Description
 2024.06.15  박수정   Created
+2024.06.15  박수정   Modified   공원 전체에 대한 지도 API 추가
 2024.06.15  박수정   Modified   지도 API 분리 - routes, service, model
+2024.06.18  박수정   Modified   공원 1개에 대한 지도 API 추가
 */
 
 import MapModel from '../models/map.js';
 import { validateQueryAndField } from '../utils/validations.js';
 
 class MapService {
-    static async getMap() {
-        const { rows } = await MapModel.getMap();
+    // 공원 전체 조회
+    static async getAllMap() {
+        const { rows } = await MapModel.getAllMap();
 
         // 쿼리 및 각 필드에 대한 유효성 검사
-        const requiredFields = ['name', 'address', 'latitude', 'longitude'];
+        const requiredFields = ['id', 'name', 'address', 'latitude', 'longitude'];
         const validatedRows = validateQueryAndField(rows, requiredFields);
 
         const resData = validatedRows.map(data => {
             return {
+                id: data.id,
                 name: data.name,
                 address: data.address,
                 latitude: data.latitude,
                 longitude: data.longitude,
             };
         });
+
+        return resData;
+    }
+
+    // 공원 1개 조회
+    static async getOneMap(id) {
+        const { rows } = await MapModel.getOneMap(id);
+
+        // 쿼리 및 각 필드에 대한 유효성 검사
+        const requiredFields = ['id', 'name', 'address', 'latitude', 'longitude'];
+        const validatedRows = validateQueryAndField(rows, requiredFields);
+
+        const data = validatedRows[0];
+        const resData = {
+            id: data.id,
+            name: data.name,
+            address: data.address,
+            latitude: data.latitude,
+            longitude: data.longitude,
+        };
 
         return resData;
     }
