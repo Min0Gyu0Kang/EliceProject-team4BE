66f6191b445db4798caf3061b1bb675d94d6bb12
youmin29 <babybana0829@naver.com>
2024-06-20 14:35:28 +0900

    🚑 Fix: verifyAuthToken 경로명 수정

--- src/routes/parkReview.js
+++ src/routes/parkReview.js
@@ -21,7 +21,7 @@ Date        Author   Status    Description
 import { Router } from 'express';
 import ParkReviewService from '../services/parkReview.js';
 import { BadRequest } from '../utils/errors.js';
-import verifyAuthToken from '../utils/auth.js';
+import verifyAuthToken from '../utils/authToken.js';
 
 const router = Router();
 
